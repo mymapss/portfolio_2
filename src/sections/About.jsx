958d7e509f323732cd14cee36e67fbee32959c17
mymapss <trendtrove58@gmail.com>
2025-02-22 16:19:48 +0530

    Project Completed

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
-            <img src="assets/laptop.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+            <img src="assets/lap-removebg-preview.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
 
             <div>
               <p className="grid-headtext">Hi, I’m Bhoomika</p>
